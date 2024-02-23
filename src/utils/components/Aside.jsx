a7a4e063ef9c8c6574588ace72c184d0f8c0ca29
noahSnbr07 <nur.noah.saschenbrecker@gmail.com>
2024-02-23 16:22:12 +0100

    aside with param routing

--- src/utils/components/Aside.jsx
+++ src/utils/components/Aside.jsx
@@ -1,7 +1,55 @@
-import React from 'react'
-
+import React from 'react';
+import '../../styles/aside.css';
+import { useSearchParams } from 'react-router-dom';
+import Icon from '../../utils/components/Icon';
 export default function Aside() {
+    const [searchFilters, setSearchFilters] = useSearchParams({
+        bestseller: false,
+        latest: false,
+        trending: false,
+        books: false,
+        tech: false,
+        sale: false,
+        sport: false,
+        clothing: false,
+        music: false,
+        movies: false,
+    });
+
+    const toggleSearchParam = (paramName) => {
+        setSearchFilters(prevParams => {
+            const currentParamValue = prevParams.get(paramName) === 'true';
+            const updatedParamValue = !currentParamValue;
+            prevParams.set(paramName, updatedParamValue.toString());
+            return prevParams;
+        });
+    };
+
+    const Filter = ({ title, isActive }) => {
+        return (
+            <div
+                onClick={() => toggleSearchParam(title.toLowerCase())}
+                className={`aside-filter ${isActive ? 'aside-filter-active' : ''}`}>
+                <h3>{title}</h3> {isActive && <Icon icon={'check'} />}
+            </div>
+        );
+    };
+
     return (
-        <aside className='aside'>Aside</aside>
-    )
+        <aside className='aside'>
+            <center>
+                <h1> Filters </h1>
+            </center>
+            <Filter title={'Bestseller'} isActive={searchFilters.get('bestseller') === 'true'} />
+            <Filter title={'Latest'} isActive={searchFilters.get('latest') === 'true'} />
+            <Filter title={'Trending'} isActive={searchFilters.get('trending') === 'true'} />
+            <Filter title={'Books'} isActive={searchFilters.get('books') === 'true'} />
+            <Filter title={'Tech'} isActive={searchFilters.get('tech') === 'true'} />
+            <Filter title={'Sale'} isActive={searchFilters.get('sale') === 'true'} />
+            <Filter title={'Sport'} isActive={searchFilters.get('sport') === 'true'} />
+            <Filter title={'Clothing'} isActive={searchFilters.get('clothing') === 'true'} />
+            <Filter title={'Music'} isActive={searchFilters.get('music') === 'true'} />
+            <Filter title={'Movies'} isActive={searchFilters.get('movies') === 'true'} />
+        </aside>
+    );
 }
--- src/utils/components/ItemsView.jsx
+++ src/utils/components/ItemsView.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import '../../styles/itemsview.css';
+import { useSearchParams } from 'react-router-dom';
+import Stock from '../../assets/libs/Stock.json';
 export default function ItemsView() {
+    const [searchFilters] = useSearchParams([]);
+
+    for (const entry of searchFilters.entries()) {
+        console.log(entry[0]);
+    }
+
     return (
-        <div className='items-view'>ItemsView</div>
-    )
+        <div className='items-view'>
+
+        </div>
+    );
 }
